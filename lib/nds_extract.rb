a875e338c06b19a1e483b33444c66e1fbc95d6cb
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-11-22 12:48:04 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -36,8 +36,8 @@ def directors_totals(nds)
   #
   # Be sure to return the result at the end!
   nil
-  # pp result[count] = total
-  pp eachTotal
+  pp result[count] = eachTotal
+  # pp eachTotal
 end
 
 # {:name=>"Spike Lee",
