80139fead5f8092970149ea474b0f397bd50cc80
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-11-22 13:12:05 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,7 +6,7 @@ def directors_totals(nds)
   # that you know what you're starting with!
   #
   #
-  pp nds[0][:movies][:worldwide_gross].class
+  pp nds[0][:movies][0][:worldwide_gross].class
   # pp nds.length
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {
